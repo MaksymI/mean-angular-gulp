0fce970679a51b76fcfa9342b3342d2eb942f420
MaksymI <maksym.izmailov.lv@gmail.com>
2017-05-12 13:28:51 +0300

    mongoose and get

--- back-end/server_mongodb.js
+++ back-end/server_mongodb.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var mongo = require('mongodb').MongoClient;
 
-
-
-var Message = mongoose.model('Message',{
-    msg: String
-});
+var database;
 
 app.use(bodyParser.json());
 
@@ -17,25 +13,18 @@ app.use(function(req,res,next){
     next();
 });
 
-app.get('/api/message', GetMessages);
-
 app.post('/api/message', function(req, res){
     console.log(req.body);
-    var message = new Message(req.body);
-    message.save();
+    database.collection('messages').insertOne(req.body);
     res.status(200);
 });
 
-function GetMessages(req, res){
-    Message.find({}).exec(function(err, result){
-        res.send(result);
-    });
-}
-
-mongoose.connect("mongodb://localhost:27017/test", function(err,db){
+mongo.connect("mongodb://localhost:27017/test", function(err,db){
     if(!err){
         console.log("We are connected to mongo");
-                  
+        database = db;
+        
+      
     }
 })
 
--- front-end/src/app/main/main.controller.js
+++ front-end/src/app/main/main.controller.js
@@ -2,8 +2,15 @@ export class MainController {
   constructor ($http) {
     'ngInject';
       this.$http = $http;
+      this.getMessages();
 
   }
+    getMessages(){
+        this.$http.get('http://localhost:5000/api/message').then(function(result){
+            console.log(result);
+        });
+    }
+    
     postMessage(){
         this.$http.post('http://localhost:5000/api/message', {msg: this.message});
     }
