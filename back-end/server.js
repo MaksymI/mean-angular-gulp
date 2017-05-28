140b5b613d958083bc29dc487e0182ba9c6a88b2
fastus <fastus1@gmail.com>
2017-05-29 01:17:25 +0300

    Middleware - checkAuthenticated and cors in separete dir services on server

--- back-end/server.js
+++ back-end/server.js
@@ -2,34 +2,24 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var jwt = require('jwt-simple');
 
 var auth = require('./controllers/auth');
 var message = require('./controllers/message');
+var checkAuthenticated = require('./services/checkAuthenticated');
+var cors = require('./services/cors');
 
+// Middleware
 app.use(bodyParser.json());
+app.use(cors);
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
-
-function checkAuthenticated(req, res, next) {
-    if (!req.header('Authorization')) {
-        return res.status(401).send({ message: 'Please make sure your reqest has an Authorization header' });
-    }
-    var token = req.header('Authorization').split('')[1];
-
-    var payload = jwt.decode(token, 'secret');
-}
-
+// Requests
 app.get('/api/message', message.get);
 
-app.post('/api/message', message.post);
+app.post('/api/message', checkAuthenticated, message.post);
 
 app.post('/auth/register', auth.register);
 
+// Connection
 mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
     if (!err) {
         console.log("We are connected to mongo");
