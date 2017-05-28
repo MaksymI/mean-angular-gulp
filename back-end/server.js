abc6c40a669d8b086fd51cecb29799338073eabf
fastus <fastus1@gmail.com>
2017-05-28 23:21:55 +0300

    auth control

--- back-end/server.js
+++ back-end/server.js
@@ -3,11 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-
-
-var Message = mongoose.model('Message',{
-    msg: String
-});
+var auth = require('./controllers/auth');
+var message = require('./controllers/message');
 
 app.use(bodyParser.json());
 
@@ -17,32 +14,18 @@ app.use(function(req,res,next){
     next();
 });
 
-app.get('/api/message', GetMessages);
+app.get('/api/message', message.get);
 
-app.post('/api/message', function(req, res){
-    console.log(req.body);
-    var message = new Message(req.body);
-    message.save();
-    res.status(200);
-});
+app.post('/api/message', message.post);
 
-app.post('/auth/register', function(req, res){
-    console.log(req.body);
-    
-});
-
-function GetMessages(req, res){
-    Message.find({}).exec(function(err, result){
-        res.send(result);
-    });
-}
+app.post('/auth/register', auth.register);
 
 mongoose.connect("mongodb://localhost:27017/test", function(err,db){
     if(!err){
         console.log("We are connected to mongo");
                   
     }
-})
+});
 
 var server = app.listen(5000, function(){
     console.log('listening on port ', server.address().port);
